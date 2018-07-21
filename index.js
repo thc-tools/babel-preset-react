43c7255cbc7c81e5a942a3b69448df33fcff7eb4
Hartorn <bazirehoussin@gmail.com>
2018-07-21 15:26:59 +0200

    feature : Adding mode option, removing development option, updating doc

--- index.js
+++ index.js
@@ -7,40 +7,69 @@
 //  ['babel-plugin-transform-runtime', { helpers: false, polyfill: false, regenerator: true }],
 //  ['babel-plugin-transform-regenerator', { async: false }],
 module.exports = (api, options, dirname) => {
-    const development = !!options.development;
+    api.assertVersion(7);
+
+    const mode = options.mode;
+    if (["development", "production", "test"].includes(mode)) {
+        throw new Error("@thc/preset-react 'mode' option must be either 'development', 'production' or 'test'.");
+    }
+    const development = mode === "development";
+    const test = mode === "test";
+    const production = mode === "production";
     const hot = !!options.hot;
+
     return {
         presets: [
-            ["@babel/preset-env", {
-                useBuiltIns: 'entry',
-                modules: false
-            }],
-            ["@babel/preset-react", {
-                useBuiltIns: true,
-                development
-            }]/*,
+            [
+                "@babel/preset-env",
+                {
+                    useBuiltIns: "entry",
+                    modules: test ? "commonjs" : false
+                }
+            ],
+            [
+                "@babel/preset-react",
+                {
+                    useBuiltIns: true,
+                    development: development || test
+                }
+            ] /*,
             !development && ["babel-preset-minify", {
                 removeConsole: true,
                 removeDebugger: true
             }]*/
         ].filter(Boolean),
         plugins: [
-            ["@babel/plugin-proposal-decorators", { "legacy": true }], //https://babeljs.io/docs/en/next/babel-plugin-proposal-decorators
-            ["@babel/plugin-proposal-class-properties", { "loose": true }],
+            ["@babel/plugin-proposal-decorators", { legacy: true }], //https://babeljs.io/docs/en/next/babel-plugin-proposal-decorators
+            ["@babel/plugin-proposal-class-properties", { loose: true }],
             ["@babel/plugin-proposal-function-bind"],
             ["@babel/plugin-proposal-object-rest-spread", { useBuiltIns: true }],
             ["@babel/plugin-proposal-optional-chaining"],
             ["@babel/plugin-proposal-nullish-coalescing-operator"],
-            ['@babel/plugin-transform-runtime', { helpers: true, polyfill: false, regenerator: false, useBuiltIns: true, useESModules: true }], //https://babeljs.io/docs/en/next/babel-plugin-transform-runtime
-            ['@babel/plugin-syntax-dynamic-import'],
+            !test && [
+                "@babel/plugin-transform-runtime",
+                //https://babeljs.io/docs/en/next/babel-plugin-transform-runtime
+                {
+                    helpers: true,
+                    polyfill: false,
+                    regenerator: false,
+                    useBuiltIns: true,
+                    useESModules: true
+                }
+            ],
+            ["@babel/plugin-syntax-dynamic-import"],
             ["babel-plugin-lodash"],
-            development && hot && ['react-hot-loader/babel'],
-            !development && ["@babel/plugin-transform-react-inline-elements"],
-            !development && ["@babel/plugin-transform-react-constant-elements"],
-            !development && ["babel-plugin-transform-react-remove-prop-types", {
-                "mode": "remove",
-                // Safe ?"ignoreFilenames": ["node_modules"] //https://github.com/oliviertassinari/babel-plugin-transform-react-remove-prop-types#ignorefilenames
-            }]
+            development && hot && ["react-hot-loader/babel"],
+            production && ["@babel/plugin-transform-react-inline-elements"],
+            production && ["@babel/plugin-transform-react-constant-elements"],
+            production && [
+                "babel-plugin-transform-react-remove-prop-types",
+                {
+                    mode: "remove"
+                    //https://github.com/oliviertassinari/babel-plugin-transform-react-remove-prop-types#ignorefilenames
+                    // Safe ?"ignoreFilenames": ["node_modules"]
+                }
+            ]
         ].filter(Boolean)
-    }
+    };
 };
